ba5b072cb1fe81203a6a1055d7ae3c8021059a2f
James Edens <jedensuscg@gmail.com>
2022-11-12 17:05:06 -0600

    more work on ToDo app

--- App 1 - ToDo/todo.py
+++ App 1 - ToDo/todo.py
@@ -1,27 +1,35 @@
 import sys
 
+def format_input(input):
+    return input.lower().strip()
 
 todos = []
 user_action = ''
 while True:
     if len(todos) == 0:
         print("No ToDo list currently exists.")
-        user_action = input("type add or exit").lower()
+        user_action = format_input(input("type add or exit\n"))
         match user_action:
             case 'add':
-                user_prompt = "Enter a ToDo Task:"
+                user_prompt = "Enter a ToDo Task:\n"
                 todo = input(user_prompt).capitalize()
                 todos.append(todo)
             case 'exit':
-                sys.exit()
+                break
     else:
-        user_action = input("type add, show or exit").lower()
+        user_action = format_input(input("type add, show, edit or exit\n"))
         match user_action:
             case 'add':
-                user_prompt = "Enter a ToDo Task:"
+                user_prompt = "Enter a ToDo Task:\n"
                 todo = input(user_prompt).capitalize()
                 todos.append(todo)
             case 'show':
-                print(todos)
+                print("** TODO LIST **")
+                for item in todos:
+                    print("-" + item)
+                print("**End of TODO list.**")
+            case 'edit':
+                print("Select the number of the item to edit")
             case 'exit':
-                sys.exit()
+                break
+print("Exiting")
