f3124f61401f9c7d64a4f55cf9ffdd81a8dcbcff
James Edens <jedensuscg@gmail.com>
2022-11-11 20:38:24 -0600

    changes to ToDo app

--- App 1 - ToDo/todo.py
+++ App 1 - ToDo/todo.py
@@ -1,26 +1,27 @@
+import sys
+
+
 todos = []
 user_action = ''
 while True:
-    if todos.length == 0:
+    if len(todos) == 0:
         print("No ToDo list currently exists.")
         user_action = input("type add or exit")
         match user_action:
             case 'add':
                 user_prompt = "Enter a ToDo Task:"
-                todos = []
                 todo = input(user_prompt)
                 todos.append(todo)
             case 'exit':
-                quit()
+                sys.exit()
     else:
-        user_action = "type add, show or exit"
+        user_action = input("type add, show or exit")
         match user_action:
             case 'add':
                 user_prompt = "Enter a ToDo Task:"
-                todos = []
                 todo = input(user_prompt)
                 todos.append(todo)
             case 'show':
                 print(todos)
             case 'exit':
-                quit()
+                sys.exit()
